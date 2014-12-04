6ea932a89a9ac1c14643543c96a525f03d24f73c
Craig B. Agricola <craig@theagricolas.org>
2014-12-04 04:25:27 -0800

    Added code to handle the URL scheme callback mechanism for the Facebook login credentials
    Apparently, the Facebook login mechanism is to launch either the Facebook app,
    if installed on the device, or Safari to do a web login, and once the login is
    complete, our application will receive the login credentials by having
    registered to handle a URL scheme which gets opened.  This version of the code
    doesn't seem to actually work, however, so more effort is going to be needed
    here.

--- BarreForestGuide/AppDelegate.m
+++ BarreForestGuide/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import <GoogleMaps/GoogleMaps.h>
+#import <FacebookSDK/FacebookSDK.h>
 #import "MapViewController.h"
 
 @interface AppDelegate ()
@@ -145,6 +146,19 @@
     } else {
         //Do stuff that you would do if the application was not active
     }
+
+- (BOOL)application:(UIApplication*)application
+            openURL:(NSURL*)url
+  sourceApplication:(NSString*)sourceApplication
+         annotation:(id)annotation
+{
+    NSLog(@"application:openUrl:sourceApplication:annotation called");
+    // Call FBAppCall's handleOpenURL:sourceApplication to handle Facebook app responses
+    BOOL wasHandled = [FBAppCall handleOpenURL:url sourceApplication:sourceApplication];
+
+    // You can add your app-specific url handling code here if needed
+
+    return wasHandled;
 }
 
 @end
